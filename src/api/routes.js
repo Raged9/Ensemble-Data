f8c2c730c9f07e65ef000c5152562c2dd58cf42c
ARYASUTA22 <aryasuta.susantio@gmail.com>
2025-04-24 23:57:00 +0700

    API youtube dari Branch-Carlo

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const customer = require('./components/customer/customer-route');
 const tiktok = require('./components/tiktok/tiktok-route');
+const youtube = require('./components/youtube/youtube-route');
 
 module.exports = () => {
   const app = express.Router();
 
   customer(app);
   tiktok(app);
+  youtube(app);
 
   return app;
 };
